f899b5dbdb2dae8d8ad1935f66615cfa285382d0
John Hurliman <jhurliman@jhurliman.org>
2022-09-08 17:52:06 -0700

    Add skipTypeFixup to ParseOptions

--- src/parse.ts
+++ src/parse.ts
@@ -16,7 +16,13 @@ import { RosMsgField, RosMsgDefinition } from "./types";
 const ROS1_GRAMMAR = Grammar.fromCompiled(ros1Rules);
 
 export type ParseOptions = {
+  /** Parse message definitions as ROS2. Otherwise, parse as ROS1 */
   ros2?: boolean;
+  /**
+   * Return the original type names used in the file, without normalizing to
+   * fully qualified type names
+   */
+  skipTypeFixup?: boolean;
 };
 
 // Given a raw message definition string, parse it into an object representation.
@@ -71,17 +77,19 @@ export function parse(messageDefinition: string, options: ParseOptions = {}): Ro
   );
 
   // Fix up complex type names
-  types.forEach(({ definitions }) => {
-    definitions.forEach((definition) => {
-      if (definition.isComplex === true) {
-        const foundName = findTypeByName(types, definition.type).name;
-        if (foundName == undefined) {
-          throw new Error(`Missing type definition for ${definition.type}`);
+  if (options.skipTypeFixup !== true) {
+    types.forEach(({ definitions }) => {
+      definitions.forEach((definition) => {
+        if (definition.isComplex === true) {
+          const foundName = findTypeByName(types, definition.type).name;
+          if (foundName == undefined) {
+            throw new Error(`Missing type definition for ${definition.type}`);
+          }
+          definition.type = foundName;
         }
-        definition.type = foundName;
-      }
+      });
     });
-  });
+  }
 
   return types;
 }
