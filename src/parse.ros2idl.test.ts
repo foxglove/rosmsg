98210010270ab10ead900405b5278f1fec469dde
snosenzo <sam@foxglove.dev>
2023-03-01 13:52:19 -0500

    - remove cruise license from newly created test file

--- src/parse.ros2idl.test.ts
+++ src/parse.ros2idl.test.ts
@@ -1,12 +1,3 @@
-// This file incorporates work covered by the following copyright and
-// permission notice:
-//
-//   Copyright 2018-2021 Cruise LLC
-//
-//   This source code is licensed under the Apache License, Version 2.0,
-//   found at http://www.apache.org/licenses/LICENSE-2.0
-//   You may not use this file except in compliance with the License.
-
 import { parse } from "./parse";
 
 describe("rosidl grammar tests", () => {
